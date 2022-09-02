9784e0307cd6734917493839f1fe2e12ec7e3f74
Janelle Wallace <janelleawallace@gmail.com>
2022-09-01 21:54:41 -0400

    This is my commit for assignment 1.

---
+++ j_wallace/src/helloWorld/HelloWorld.java
@@ -0,0 +1,12 @@
+package helloWorld;
+
+/**
+ * Janelle Wallace
+ * @author janellewallace
+ *
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello Large Scale Programming!!");
+	}
+}
